281420c639997a449bb058fd357c41c911467ede
Andy Marks <vampwillow@gmail.com>
2019-06-10 10:26:01 +1000

    Playing with destination generation

--- src/house.sh
+++ src/house.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-WIDTH=${1:-0}
-BREADTH=${2:-0}
-DEPTH=${3:-0}
+WIDTH=${1}
+BREADTH=${2}
+DEPTH=${3}
+
+generate_destination() {
+    DEST=( $(( ( RANDOM % WIDTH )  + 1 )) $(( ( RANDOM % BREADTH )  + 1 )) $(( ( RANDOM % DEPTH )  + 1 )) )
+}
 
 echo "Building Game with dimensions $WIDTH $BREADTH $DEPTH"
+generate_destination
+echo "You need to find your way to (${DEST[*]})"
